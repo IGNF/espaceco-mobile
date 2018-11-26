28c4c11bd430b650a3531a17119d63a4fa1eda85
JMViglino <jean-marc.viglino@ign.fr>
2018-11-26 10:47:53 +0100

    [VER] v1.1.9

--- www/js/config.js
+++ www/js/config.js
@@ -9,7 +9,7 @@ apiKey = "2wd9sfsi5hews9yxcr95i6br";
 auth = 'Z3VpY2hldDpFc3BhY2VDMDgwNjk=';
 
 // App version
-CordovApp.prototype.version = "1.1.09b";
+CordovApp.prototype.version = "1.1.09";
 
 // Autoconfig GPP
 geoportailConfig.capabilities['default']=
