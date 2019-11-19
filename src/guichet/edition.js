bace67e3eec067b1fac13ef0317a17f7f83fb277
JMViglino <jean-marc.viglino@ign.fr>
2019-11-19 17:13:31 +0100

    [UPD] edition attributaire

--- src/guichet/edition.js
+++ src/guichet/edition.js
@@ -6,23 +6,126 @@ import ol_ext_element from 'ol-ext/util/element'
  * @param {Element} ul liste element
  * @param {ol.Feature} feature current feature
  * @param {*} ftype featureType attribute
- * @param {number} att current attribut name
+ * @param {string} att current attribut name
  */
 function _addLine (ul, feature, ftype, att) {
-  // if (ftype.readOnly) return;
+  if (ftype.readOnly) return;
   console.log(ftype);
   const li = ol_ext_element.create('LI', {
+    className: 'edition '+ftype.type,
     parent: ul
   });
-  ol_ext_element.create('LABEL', {
+  // Label
+  const label = ol_ext_element.create('LABEL', {
     html: ftype.title,
     parent: li
   });
-  ol_ext_element.create('INPUT', {
-    value: feature.get(att),
+  let error = '';
+  ol_ext_element.create('DIV', {
+    className: 'error',
+    click: () => {
+      if (error) {
+        wapp.alert(error);
+      }
+    },
     parent: li
   });
-
+  // Input
+  let input;
+  switch (ftype.type) {
+    // Chaine de caractères
+    case 'String': {
+      input = ol_ext_element.create('INPUT', {
+        value: feature.get(att),
+        parent: li
+      });
+      ['keyup','change','input'].forEach((e) => {
+        input.addEventListener(e, () => {
+          if (input.value.length > ftype.max_length) {
+            li.classList.add('error');
+            error = 'Chaine trop longue ('+ftype.max_length+' caractères maxi.)...';
+          } else {
+            li.classList.remove('error');
+            error = '';
+          }
+        });
+      });
+      break;
+    }
+    // Boolean
+    case 'Boolean': {
+      ol_ext_element.create('INPUT', {
+        value: feature.get(att),
+        type: 'checkbox',
+        on: {
+          change: () => { console.log('ok') }
+        },
+        parent: label
+      });
+      ol_ext_element.create('SPAN', {
+        parent: label
+      });
+      break;
+    }
+    case 'Choice': {
+      const elt = ol_ext_element.create('INPUT', {
+        value: feature.get(att),
+        disabled: "disabled",
+        parent: li
+      });
+      li.addEventListener('click', () => {
+        const values = {};
+        ftype.listOfValues.forEach((v)=>{
+          if (v===null) values[''] = '<i>indéfini</i>';
+          else values[v] = v;
+        })
+        wapp.selectDialog(values, elt.value, (val) => {
+          elt.value = val;
+        })
+      });
+      break;
+    }
+    case 'Date':
+    case 'DateTime': {
+      ol_ext_element.create('INPUT', {
+        value: feature.get(att),
+        type: (ftype.type==='Date' ? 'date' : 'datetime-local'),
+        parent: li
+      });
+      break;
+    }
+    case 'Integer':
+    case 'Double': {
+      input = ol_ext_element.create('INPUT', {
+        value: feature.get(att),
+        type: 'number',
+        parent: li
+      });
+      ['keyup','change','input'].forEach((e) => {
+        input.addEventListener(e, () => {
+          if (input.value!==0 && !input.value) {
+            li.classList.add('error');
+            error = '"'+att+'" doit être un nombre valide...';
+          } else if (ftype.type === 'Integer' && parseInt(input.value) !== parseFloat(input.value)) {
+            li.classList.add('error');
+            error = '"'+att+'" doit être un entier...';
+          } else {
+            li.classList.remove('error');
+            error = '';
+          }
+        });
+      });
+      break;
+    }
+    default: {
+      ol_ext_element.create('INPUT', {
+        value: feature.get(att),
+        readOnly: 'readOnly',
+        parent: li
+      });
+      break;
+    }
+  }
 };
 
 /** Edit current feature
@@ -46,7 +149,7 @@ wapp.editFeature = function() {
   
   // Formulaire
   var i, att;
-  for (i in ftype.attributes) if (i !== ftype.geometryName && feature.get(i)) {
+  for (i in ftype.attributes) if (i !== ftype.geometryName) {
     att = ftype.attributes[i];
     switch (att.type) {
       case 'Point':
