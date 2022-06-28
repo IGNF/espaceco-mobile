c0907766e159cfb3abaa0a2f0d82ecd8a50524ee
alexia.chang <Alexia.Chang-Wailing@ign.fr>
2022-06-28 15:16:53 +0200

    naviforest : logo, userinfo

--- naviforest/src/index.js
+++ naviforest/src/index.js
@@ -20,19 +20,37 @@ document.addEventListener("DOMContentLoaded", init, false);
     setTimeout(() => { 
         console.log("onload");
         
-        // wapp.help.reset();
-        // wapp.hidePage();
-        $("div[data-role='menu'] .logo.back").hide();
-         $("div[data-role='menu'] .img").css({'background-color': 'rgba(255,255,255,0)'});
+        wapp.help.reset();
+        wapp.hidePage();
+        //$("div[data-role='menu'] .logo.back").hide();
+        //  $("div[data-role='menu'] .img").css({'background-color': 'rgba(255,255,255,0)'});
          $('<li onclick=\'console.log("arretés")\')><i class="fa fa-file-pdf-o"></i>Arrêtés préfect</li>').insertAfter("div[data-role='menu'] ul li.guichet");
         $('head title').html("Naviforest");
        
-        // $("button i.fa.tools-locate").prop('disabled', false);
-        // $("button i.fa.tools-locate").trigger('click');
-        // wapp.help.show('main');
+        $("button i.fa.tools-locate").prop('disabled', false);
+        $("button i.fa.tools-locate").trigger('click');
+        wapp.help.show('main');
         $('.buttons.changeGroupe button.button').hide();
+        $('#changeGuichet').hide();
+
+
+        $('#headerBackground').hide();
+
+        $('p.userinfo').css({'color' :'#888'});
+        if ($('span.connected').html() === "") {
+          $('p.userinfo').hide();
+        } else {
+          $('p.userinfo').show();       
+        }
         
-      }, 1000);
+
+        $("[data-role='menu'] .img ").css({'border': 'none','border-radius':0, 'box-shadow':'none',
+        'background-color': 'rgba(255,255,255,0)', 'width':'50% !important','height':'50% !important' , 'left':'0px', 'top': 0});
+        $("[data-role='menu'] p ").css({'left':'4.6em','width':'40% !important'});
+
+  
+        
+      }, 500);
   }
 
    
