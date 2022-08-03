bc04403bc855e58a3b9b9099010094ac55271265
alexia.chang <Alexia.Chang-Wailing@ign.fr>
2022-08-03 13:59:14 +0200

    nf : arrete selon département

--- naviforest/src/jsNF/arretes.js
+++ naviforest/src/jsNF/arretes.js
@@ -1,6 +1,6 @@
 import wapp from '../wapp';
 import { toLonLat } from 'ol/proj';
-import * as Gp from './GpServices.js';
+
 
 
 wapp.getArrete = function (n) {  
@@ -31,14 +31,14 @@ wapp.getArrete = function (n) {
       console.log('then');
      var arrete ="";
     
-      if (result.locations.length > 0) {
-        let nodep =result.locations[0].placeAttributes.department;
-        arrete = "https://naviforest.ign.fr/arretes/" + nodep + "_arrete_transport_bois.pdf ";
-         console.log(arrete);
+      // if (result.locations.length > 0) {
+      //   let nodep =result.locations[0].placeAttributes.department;
+      //   arrete = "https://naviforest.ign.fr/arretes/" + nodep + "_arrete_transport_bois.pdf ";
+      //    console.log(arrete);
        
-        }
+      //   }
    
-        window.open("https://naviforest.ign.fr", "_system");
+      // //  window.open("https://naviforest.ign.fr", "_system");
      
       
     })
@@ -54,34 +54,52 @@ wapp.getArrete = function (n) {
 
 function go(lat,lon,fo) {
 
-   
+    var params= 'searchgeom={"type":"Circle","coordinates":['+lat+','+lon +'],"radius":100}&' +
+                'lon='+lon+'&lat='+lat +
+                'index=poi&category=département&limit=1';
     return new Promise(function(resolve, reject) {
       wapp.wait("chargement ...");
-      Gp.Services.reverseGeocode({
-        position: {
-          x: lat,
-          
-          y: lon 
-        },
-        srs: 'EPSG:4326',
-        filterOptions: fo,
-        apiKey: "calcul",
-        onSuccess: function(result) {
-       
-          wapp.wait(false);
 
-          resolve(result);
-      
+      $.ajax({
+        //L'URL de la requête 
+        // url: "https://wxs.ign.fr/essentiels/geoportail/geocodage/rest/0.1/reverse?"+ 
+        //        params,
 
-        },
-        onFailure: function(error) {
-         console.log(error);
-         $("body").css("cursor", "default");
-         alert("erreur");
-         wapp.wait(false);
-        reject(error);
-        }
-      });
+        url: "https://api-adresse.data.gouv.fr/reverse/?lon="+ lon+"&lat="+lat,
+
+        //La méthode d'envoi (type de requête)
+        method: "GET",
+
+        //Le format de réponse attendu
+        dataType : "json",
+    })
+    //Ce code sera exécuté en cas de succès - La réponse du serveur est passée à done()
+    /*On peut par exemple convertir cette réponse en chaine JSON et insérer
+     * cette chaine dans un div id="res"*/
+    .done(function(response){
+        let data = JSON.stringify(response);
+       if (response.features.length>0) {
+        let dep = response.features[0].properties.postcode.substr(0,2);
+        alert('https://naviforest.ign.fr/arretes/'+dep+ '_arrete_transport_bois.pdf');
+       } else {
+        alert('https://naviforest.ign.fr/arretes/');
+       }
+        console.log(data);
+    })
+
+    //Ce code sera exécuté en cas d'échec - L'erreur est passée à fail()
+    //On peut afficher les informations relatives à la requête et à l'erreur
+    .fail(function(error){
+        alert("La requête s'est terminée en échec. Infos : " + JSON.stringify(error));
+    })
+
+    //Ce code sera exécuté que la requête soit un succès ou un échec
+    .always(function(){
+      //  alert("Requête effectuée");
+        wapp.wait(false);
+
+    });
+   
     });
    
 
