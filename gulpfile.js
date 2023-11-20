09e301afb443d38c55bf79d4ebbf19affc8d2257
Madeline Jacopucci <madeline.jacopucci@ign.fr>
2023-11-20 10:04:30 +0100

    upgrade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,13 @@ gulp.task('doc', function (cb) {
     var config = require('./jsdoc.json');
     gulp.src([
 		"./doc/readme.md", "./doc/*.js", 
-		"./www/js/wapp/*.js",
-		"./www/js/guichet.js",
-		"./www/js/wapp.guichet.js",
-		"./www/js/wapp.ripart.js",
-		"./www/js/ripart.js"
+		"./src/wapp.js",
+		"./src/guichet/*.js",
+		"./src/report/*.js",
+		"./src/map/*.js",
 	], {read: false})
         .pipe(jsdoc(config, cb));
 });
 
 // The default task that will be run if no task is supplied
-gulp.task("default", ["doc"]);
+gulp.task("default", gulp.series("doc"));
