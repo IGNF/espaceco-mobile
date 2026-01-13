29473647821a7ece3ec7a1b6668fa2e5fe95b528
Magali Perceval <magali.perceval@ign.fr>
2026-01-13 15:26:52 +0100

    Chnage version number for Naviforest
    Signed-off-by: Magali Perceval <magali.perceval@ign.fr>

--- scripts/NaviForest/config.js
+++ scripts/NaviForest/config.js
@@ -3,7 +3,7 @@ export default {
   guichetID: 658, // naviforest
   name: "Naviforest",
   displayName: "Naviforest",
-  versionNumber: "2.0.6",
+  versionNumber: "3.1.0",
   ios:{
     bundleId: "fr.ign.navi-forest"
   },
